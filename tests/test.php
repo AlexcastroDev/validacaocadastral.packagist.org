627e84a6a90e0fe749095c7032b52f172f38ea94
admin <admin@MacBook-Pro-de-admin.local>
2020-06-30 23:28:27 -0300

    update test with endpoint

--- tests/test.php
+++ tests/test.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
-use App\ValidacaoCadastral;
+use H1code\ValidacaoCadastral;
 
 final class ValidacaoCadastralTest extends TestCase
 {
@@ -23,11 +23,27 @@ final class ValidacaoCadastralTest extends TestCase
     {
         $this->assertEquals(
             [
-                "fantasia"=> "validacao cadastral",
-                "razao"=> "validacao cadastral",
-                "cnpj"=> "18.611.939/0001-58",
-                "credit"=> 100,
-                "status"=> 1
+                
+                    "razao"=> "validacao cadastral",
+                    "fantasia"=> "validacao cadastral",
+                    'email' => 'validacao@mail.com.br',
+                   'cnpj' => "00.000.000/0001-00",
+                    "situacao"=> "Ativa",
+                    "telefones"=> [
+                      "(21) 00000-0000"
+                    ],
+                    "matrizEndereco"=> [
+                      "bairro"=> "validacao cadastra",
+                      "cep"=> "validacao cadastra",
+                      "logradouro"=> "validacao cadastra",
+                      "numero"=> "validacao cadastra",
+                      "complemento"=> "validacao cadastra",
+                      "cidade"=> "validacao cadastra",
+                      "uf"=> "validacao cadastra"
+                    ],
+                    "status"=> 1,
+                    "credit"=> 100
+                  
                 ],
             json_decode(ValidacaoCadastral::get('28a7976f9df7b08f87dcce30be04e60e', '18611939000158'), true)
         );
